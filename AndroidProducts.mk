45898bccabc7d6182b8456f599e030c4a0cd5375
DarkPlayer <darek.duong@gmail.com>
2021-05-07 20:57:04 +0200

    walleye: waveos bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_walleye.mk
+    $(LOCAL_DIR)/wave_walleye.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_walleye-userdebug
+    wave_walleye-userdebug
